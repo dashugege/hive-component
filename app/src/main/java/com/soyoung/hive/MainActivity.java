d083435fe2adc31413b6f43ce9856f9ae2d91154
hongjiang <hongjiang@soyoung.com>
2018-12-17 15:05:18 +0800

    测试分支提交

--- app/src/main/java/com/soyoung/hive/MainActivity.java
+++ app/src/main/java/com/soyoung/hive/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 //        https://blog.csdn.net/guiying712/article/details/55213884
 
+//        setContentView(R.layout.activity_main); test branch
 
 
 
