041fac3a742f6fc64d4a0b84dce67967232feb2f
hongjiang <hongjiang@soyoung.com>
2018-12-19 11:04:25 +0800

    修改版本号

--- app/src/main/java/com/soyoung/hive/MainActivity.java
+++ app/src/main/java/com/soyoung/hive/MainActivity.java
@@ -1,6 +1,7 @@
 package com.soyoung.hive;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        
         findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
